9be1b9a133d17068f20cf23ca712c0d1f1421603
mlodywilkuuu <mlodywilkuu@gmail.com>
2026-05-30 20:35:47 +0200

    Update engine.js

--- engine.js
+++ engine.js
@@ -1,64 +1,33 @@
-class DomestosEngine {
-    constructor(container) {
-        this.container = container;
-        this.styles = {}; // Nasz CSSOM (CSS Object Model)
-    }
+let tabs = [{ content: "<h1>Witaj w Domestos Browser</h1>" }];
+let activeTabIndex = 0;
 
-    // 1. Parser: Rozbija tekst na DOM i ekstrahuje style
-    parse(html) {
-        const parser = new DOMParser();
-        const doc = parser.parseFromString(html, 'text/html');
-        
-        // Wyciągamy style ze znacznika <style>
-        const styleTag = doc.querySelector('style');
-        if (styleTag) {
-            this.parseCSS(styleTag.textContent);
-            styleTag.remove();
-        }
-        
-        return doc.body;
-    }
+function newTab() {
+    tabs.push({ content: "<h1>Nowa karta</h1>" });
+    renderTabs();
+    switchTab(tabs.length - 1);
+}
 
-    // 2. CSS Parser: Bardzo prosta konwersja tekstu CSS na obiekt JS
-    parseCSS(cssText) {
-        const rules = cssText.split('}');
-        rules.forEach(rule => {
-            const [selector, body] = rule.split('{');
-            if (selector && body) {
-                const cleanSelector = selector.trim();
-                const declarations = body.trim().split(';');
-                this.styles[cleanSelector] = {};
-                declarations.forEach(dec => {
-                    const [prop, val] = dec.split(':');
-                    if (prop && val) this.styles[cleanSelector][prop.trim()] = val.trim();
-                });
-            }
-        });
-    }
+function switchTab(index) {
+    activeTabIndex = index;
+    renderTabs();
+    document.getElementById('viewport').innerHTML = tabs[index].content;
+}
 
-    // 3. Renderowanie z aplikowaniem stylów
-    applyEngineStyles(node, parent) {
-        if (node.nodeType === Node.ELEMENT_NODE) {
-            const el = document.createElement('div');
-            el.className = "engine-node";
-            
-            // Nakładanie stylów z naszego "CSSOM"
-            const tag = node.tagName.toLowerCase();
-            if (this.styles[tag]) {
-                Object.assign(el.style, this.styles[tag]);
-            }
+function renderTabs() {
+    const tabBar = document.getElementById('tab-bar');
+    tabBar.innerHTML = '';
+    tabs.forEach((tab, i) => {
+        const div = document.createElement('div');
+        div.className = `tab ${i === activeTabIndex ? 'active' : ''}`;
+        div.innerText = `Karta ${i + 1}`;
+        div.onclick = () => switchTab(i);
+        tabBar.appendChild(div);
+    });
+    tabBar.innerHTML += '<button onclick="newTab()">+</button>';
+}
 
-            parent.appendChild(el);
-            node.childNodes.forEach(child => this.applyEngineStyles(child, el));
-        } else if (node.nodeType === Node.TEXT_NODE && node.textContent.trim()) {
-            parent.appendChild(document.createTextNode(node.textContent));
-        }
-    }
-
-    render(html) {
-        this.container.innerHTML = '';
-        this.styles = {}; // Reset stylów przy nowym renderowaniu
-        const dom = this.parse(html);
-        this.applyEngineStyles(dom, this.container);
-    }
+function loadUrl() {
+    const val = document.getElementById('url-bar').value;
+    tabs[activeTabIndex].content = val; // Zapisuje "kod" do karty
+    document.getElementById('viewport').innerHTML = val;
 }
